a40ee0a7ff85dbb1caf7931e43fb312840e75c1c
Surya Singh <ssurya5599@gmail.com>
2021-03-19 22:18:05 -0700

    convert form to source code

--- code/java/compile.sh
+++ code/java/compile.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rm -rf bin/*.class
-javac -cp ".;lib/postgresql-42.1.4.jar;" -sourcepath src -d classes src/*.java -d bin/
+javac -cp lib/*:. -sourcepath src -d classes src/*.java -d bin/
